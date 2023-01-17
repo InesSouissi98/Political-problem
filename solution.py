2dac9cffa5f90f3077f35a3d7dd32a5fe25c974a
InesSouissi98 <122887770+InesSouissi98@users.noreply.github.com>
2023-01-17 14:45:21 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,12 @@
+import numpy as np
+from scipy.optimize import linprog
 
+A= np.array([[2,-8,0,-10],[-5,-2,0,0],[-3,5,10,2]])
+b= np.array([-50,-100,-25])
+c= np.array([1,1,1,1])
+
+solution= linprog(c=c,A_ub=A,b_ub=b, method= 'simplex',bounds=(0,None))
+
+solution.fun
+
+solution.x
